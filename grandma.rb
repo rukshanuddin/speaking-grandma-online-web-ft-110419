5603bfe96617aefb8d9a6358ed01e8ba4924a25a
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-04 14:46:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase != phrase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase == phrase
+    return "NO, NOT SINCE 1938!"
   end
 end
